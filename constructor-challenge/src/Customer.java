e4eeddf1f3aa852c14ee4a2db4291bad0c02c740
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-10 22:26:55 +0900

    feat: implement Customer class with multiple constructors and test functionality in main class
    Customer.java:
      - Added 'Customer' class with attributes: name, creditLimit, and email.
      - Implemented three constructors to provide flexibility:
          1. Full constructor with all parameters.
          2. Constructor with default creditLimit.
          3. Default constructor with pre-set values.
      - Created getter methods for all attributes.
    
    main.java:
      - Created 'main' class to test 'Customer' functionality.
      - Instantiated multiple 'Customer' objects with different constructors.
      - Verified functionality by printing customer details.

--- constructor-challenge/src/Customer.java
+++ constructor-challenge/src/Customer.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Customer {
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public Customer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public Customer(String name, String email) {
+        this(name, 1000, email);
+    }
+
+    public Customer() {
+        this("Default Name", "default@example.com");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- constructor-challenge/src/main.java
+++ constructor-challenge/src/main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("John", 1000, "johndoe@mail.com");
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmail());
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getEmail());
+
+        Customer thirdCustomer = new Customer("Jane", "jane@email.com");
+        System.out.println(thirdCustomer.getName());
+        System.out.println(thirdCustomer.getCreditLimit());
+        System.out.println(thirdCustomer.getEmail());
+    }
 }
