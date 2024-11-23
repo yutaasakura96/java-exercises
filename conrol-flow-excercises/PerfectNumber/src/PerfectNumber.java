1f15fa6a8cd70e9b728dba0d18a5465ee3e6df82
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-23 23:26:59 +0900

    Perfect number excercise

---
+++ conrol-flow-excercises/PerfectNumber/src/PerfectNumber.java
@@ -0,0 +1,24 @@
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6)); // true
+        System.out.println(isPerfectNumber(28)); // true
+        System.out.println(isPerfectNumber(5)); // false
+        System.out.println(isPerfectNumber(-1)); // false
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
+    }
+}
